0d1066301d184162a7c31ccd25828b42edfe1fdf
MarSavar <msavar03@dcs.bbk.ac.uk>
2020-06-12 17:47:25 +0100

    Initial commit

---
+++ src/main/java/LondonAPI/London/LondonApplication.java
@@ -0,0 +1,13 @@
+package LondonAPI.London;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LondonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LondonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/LondonAPI/London/LondonApplicationTests.java
@@ -0,0 +1,13 @@
+package LondonAPI.London;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LondonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
